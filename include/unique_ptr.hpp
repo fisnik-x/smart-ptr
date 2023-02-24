1732ed0ae45134c01747d8d4fb2b9d5f178df519
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-02-24 12:39:29 +0100

    Updated unique_ptr

--- include/unique_ptr.hpp
+++ include/unique_ptr.hpp
@@ -14,13 +14,14 @@
 #pragma once
 
 #include <cassert>
-#include <algorithm>
+#include <memory>
 
-template<typename T>
+template<typename T, typename D = std::default_delete<T>>
 class unique_ptr{
 public:
 	constexpr unique_ptr() noexcept = default;
 	explicit unique_ptr(T* data) : m_ptr{data}{}
+	explicit unique_ptr(T*, D){}
 	unique_ptr(unique_ptr&& other) noexcept 
 		: m_ptr{other.m_ptr} {}
 	~unique_ptr() {}
@@ -29,6 +30,7 @@ public:
 	unique_ptr& operator=(unique_ptr&& other) {}
 
 	T* get() const noexcept{}
+	D get_deleter const noexcept{}
 	T* operator*() const noexcept{}
 	T* operator->() const noexcept{}
 	explicit operator* bool() const explicit{}
@@ -39,6 +41,7 @@ public:
 
 private:
 	T* m_ptr{nullptr};
+	D m_deleter;
 };
 
 template<class T> void swap(unique_ptr<T>& lhs, unique_ptr<T> rhs) noexcept {}
