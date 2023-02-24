50bdd07c3e482490d9d092c2c2584cf1d6f8deac
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-02-24 13:50:07 +0100

    Implemented weak_ptr

--- include/weak_ptr.hpp
+++ include/weak_ptr.hpp
@@ -18,20 +18,65 @@
 
 template<typename T>
 class weak_ptr{
+    template<typename X> friend class shared_ptr;
 public:
     constexpr weak_ptr() noexcept : m_ptr{}, m_ctrlBlock{} {}
     weak_ptr(T* other) : m_ptr{other}, m_ctrlBlock{new Control_Block} { increment_weakptr(); }
+    
     weak_ptr(const shared_ptr<T>& other) noexcept 
         : m_ptr{other.m_ptr}, m_ctrlBlock{other.m_ctrlBlock} 
-    {}
-    ~weak_ptr() {}
+    {
+        increment_weakptr();
+    }
+    
+    ~weak_ptr() { decrement_weakptr(); }
 
-    void reset() noexcept {}
-    bool expired() noexcept {}
-    auto lock() {}
+    void reset() noexcept { decrement_weakptr(); }
+    
+    bool expired() noexcept 
+    {
+        if(m_ctrlBlock == nullptr)
+            return true;
+        
+        if(m_ctrlBlock->m_refCount == 0)
+            --m_ctrlBlock->m_weakCount;
+        
+        if(m_ctrlBlock->m_refCount + m_ctrlBlock->m_weakCount == 0){
+            delete m_ctrlBlock;
+            m_ptr = nullptr;
+            m_ctrlBlock = nullptr;
+        }
+
+        return !m_ctrlBlock || m_ctrlBlock->m_weakCount == 0;
+    }
+
+    auto lock() 
+    {
+        return expired() ? shared_ptr<T>() : shared_ptr<T>(*this);
+    }
 
     WeakPtr& operator=(const weak_ptr& other) 
-    weak_ptr& operator=(const shared_ptr<T>& other){}
+    {
+        if(this != &other){
+            decrement_weakptr();
+            m_ptr = other.m_ptr;
+            m_ctrlBlock = other.m_ctrlBlock;
+            increment_weakptr();
+        }
+
+        return *this;
+    }
+
+    weak_ptr& operator=(const shared_ptr<T>& other)
+    {
+        m_ptr = other.m_ptr;
+        m_ctrlBlock = other.m_ctrlBlock;
+
+        if(m_ptr != nullptr && m_ctrlBlock != nullptr)
+            ++m_ctrlBlock->m_weakCount;
+
+        return *this;
+    }
     
     template<class T> friend void swap(weak_ptr<T>& lhs, weak_ptr<T>& rhs) noexcept;
 
@@ -39,9 +84,23 @@ private:
     T* m_ptr{nullptr};
     Control_Block* m_ctrlBlock{nullptr};
     
-    void increment_weakptr(){}
-    void decrement_weakptr(){}
+    void increment_weakptr(){ ++m_ctrlBlock->m_weakCount; }
+    void decrement_weakptr()
+    {
+        if(m_ctrlBlock && m_ptr){
+            if(--m_ctrlBlock->m_weakCount == 0 && m_ctrlBlock->m_refCount == 0){
+                delete m_ctrlBlock;
+            }
+            m_ctrlBlock = nullptr;
+            m_ptr = nullptr;
+        }
+    }
 };
 
 template<class T>
-inline void swap(weak_ptr<T>& lhs, weak_ptr<T>& rhs) noexcept{}
+inline void swap(weak_ptr<T>& lhs, weak_ptr<T>& rhs) noexcept
+{
+    T temp(std::move(lhs));
+    lhs = std::move(rhs);
+    rhs = std::move(temp);
+}
