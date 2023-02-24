e6bf5b58dfc4614aa85411404d2d43439439c6ac
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-02-24 12:40:37 +0100

    Update unique_ptr.hpp

--- include/unique_ptr.hpp
+++ include/unique_ptr.hpp
@@ -4,11 +4,11 @@
     Author: Fisnik
     Copyright (C) Fisnik. All rights reserved.
 
-	Abstract:
-	
-	
-
-	This source code is licensed under the MIT license found in the
+    Abstract:
+    
+    
+    
+    This source code is licensed under the MIT license found in the
     LICENSE file in the root directory of this source tree. 
 --*/
 #pragma once
